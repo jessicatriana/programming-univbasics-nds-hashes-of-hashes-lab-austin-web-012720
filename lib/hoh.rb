745347ac8e8bfd710f37ac5c2b317825e169d417
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-21 03:45:05 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -6,14 +6,6 @@
 #
 naming_system = {
   PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-}
   
 }
 # PORTION_1 = {
