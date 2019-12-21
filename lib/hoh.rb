f8445bd3c13f30ebceef43349af9c98a21f37b64
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-21 03:43:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,7 +4,18 @@
 #
 # The tests will guide your construction.
 #
-naming_system = {}
+naming_system = {
+  PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
+  
+}
 # PORTION_1 = {
 #   label: "Kingdom",
 #   sub_category: {
